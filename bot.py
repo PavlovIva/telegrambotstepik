8c7f16f6ca575daddddcb4498ec2a1be62849521
Pavlov Ivan <pivaq4530@gmail.com>
2023-02-28 07:56:09 +0300

    This is first commiting!

---
+++ bot.py
@@ -0,0 +1,41 @@
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
+
+from config import TOKEN
+import emoji
+
+negan1 = 'negan1.jpeg'
+negan2 = 'negan2'
+negan3 = 'negan3'
+negan4 = 'negan4'
+
+
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=['start'])
+async def process_start_command(message: types.Message):
+    await message.reply('Hello! \nWrite something to me!')
+
+@dp.message_handler(commands=['help'])
+async def process_help_command(message: types.Message):
+    await message.reply('Write something to me and I will answer with the same text!')
+
+@dp.message_handler(commands=['voice'])
+async def process_start_command(message: types.Message):
+    await bot.send_voice(message.from_user.id, negan1, reply_to_message_id=message.message_id)
+
+@dp.message_handler()
+async def echo_message(msg: types.Message):
+    await bot.send_message(msg.from_user.id, msg.text)
+
+if __name__ == '__main__':
+    executor.start_polling(dp)
+
+
+
+
+
+
---
+++ config.py
@@ -0,0 +1,4 @@
+TOKEN = '6194869978:AAGbhg-li7xlJPFs9c2RgXzvRfui6Nt5Vxw'
+MY_ID = 77777
+
+DB_FILENAME = 'botuploads.db'
